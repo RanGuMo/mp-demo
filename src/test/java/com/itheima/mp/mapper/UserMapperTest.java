8bcf767628e29d4309c62c976f9f827dc5e4f329
zack <1126146235@qq.com>
2023-12-11 16:30:02 +0800

    feat(MyBatisPlus-核心功能): ✨ QueryWrapper 例子

--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +27,13 @@ class UserMapperTest {
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
         // userMapper.saveUser(user); //mybatis
-        userMapper.insert(user); //mybatisPlus
+        userMapper.insert(user); // mybatisPlus
     }
 
     @Test
     void testSelectById() {
         // User user = userMapper.queryUserById(5L); //mybatis
-        User user = userMapper.selectById(5L);//mybatisPlus
+        User user = userMapper.selectById(5L);// mybatisPlus
         System.out.println("user = " + user);
     }
 
@@ -40,7 +41,7 @@ class UserMapperTest {
     @Test
     void testQueryByIds() {
         // List<User> users = userMapper.queryUserByIds(List.of(1L, 2L, 3L, 4L)); //mybatis
-        List<User> users = userMapper.selectBatchIds(List.of(1L, 2L, 3L, 4L));//mybatisPlus
+        List<User> users = userMapper.selectBatchIds(List.of(1L, 2L, 3L, 4L));// mybatisPlus
         users.forEach(System.out::println);
     }
 
@@ -50,18 +51,44 @@ class UserMapperTest {
         user.setId(5L);
         user.setBalance(20000);
         // userMapper.updateUser(user); //mybatis
-        userMapper.updateById(user);//mybatisPlus
+        userMapper.updateById(user);// mybatisPlus
     }
 
     @Test
     void testDeleteUser() {
         // userMapper.deleteUser(5L); //mybatis
-        userMapper.deleteById(5L); //mybatisPlus
+        userMapper.deleteById(5L); // mybatisPlus
     }
 
     @Test
     void testQuery() {
         User user = userMapper.queryById(1L);
         System.out.println("user = " + user);
+    }
+
+    // 查询出名字中带o的，存款大于等于1000元的人。
+    @Test
+    void testQueryWrapper() {
+        // 1.构建查询条件 where name like "%o%" AND balance >= 1000
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .select("id", "username", "info", "balance")
+                .like("username", "o")
+                .ge("balance", 1000);
+        // 2.查询数据
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+
+    }
+
+    // 更新用户名为jack的用户的余额为2000
+    @Test
+    void testUpdateByQueryWrapper() {
+        // 1.构建查询条件 where name = "Jack"
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .eq("username","Jack");
+        // 2.更新数据，user中非null字段都会作为set语句
+        User user = new User();
+        user.setBalance(2000);
+        userMapper.update(user, wrapper);
     }
 }
