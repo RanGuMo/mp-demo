dcdcd19108e3384e4f0ca8378f16b3b331c4f223
zack <1126146235@qq.com>
2023-12-13 11:04:12 +0800

    feat(MyBatisPlus-核心功能): ✨ LambdaQueryWrapper和LambdaUpdateWrapper以及自定义sql 的使用

--- src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
+import javax.management.Query;
 import java.util.List;
 
 // public interface UserMapper{
@@ -21,6 +24,9 @@ import java.util.List;
 
 public interface UserMapper extends BaseMapper<User>{
     User queryById(@Param("id") Long id);
+
+    @Select("UPDATE  user SET balance = balance -#{money} ${ew.customSqlSegment}")
+    void deductBalanceByIds(@Param("money") int money, @Param("ew") QueryWrapper<User> wrapper);
 }
 
 
--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,6 +1,8 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
@@ -109,5 +111,46 @@ class UserMapperTest {
         // 而是基于UpdateWrapper中的setSql来更新
         userMapper.update(null, userUpdateWrapper);
     }
+
+   //======================== LambdaQueryWrapper ==================================================================
+   //  查询出名字中带o的，存款大于等于1000元的人。
+    @Test
+    void testLambdaQueryWrapper() {
+        // 1.构建查询条件 where name like "%o%" AND balance >= 1000
+        LambdaQueryWrapper<User> userLambdaQueryWrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId, User::getUsername, User::getInfo, User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+        // 2.查询数据
+        List<User> users = userMapper.selectList(userLambdaQueryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    // 更新用户名为jack的用户的余额为2000
+    @Test
+    void testLambdaUpdateByQueryWrapper() {
+        // 1.构建查询条件 where name = "Jack"
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .eq(User::getUsername, "Jack");
+        // 2.更新数据，user中非null字段都会作为set语句
+        User user = new User();
+        user.setBalance(2000);
+        userMapper.update(user, wrapper);
+    }
+    //===============================自定义sql=========================================================
+    // 更新id为1,2,4的用户的余额，扣200
+    @Test
+    void testCustomWrapper() {
+        // List<Long> ids = new ArrayList<>(Arrays.asList(1L, 2L, 4L)); //JDK8 可以使用这种方式
+        List<Long> ids = List.of(1L, 2L, 4L); // JDK9 之后才有的of方法
+        // 1.准备自定义查询条件
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                // .setSql("balance = balance -200")
+                .in("id", ids); // WHERE id in (1, 2, 4)
+
+        // 2.调用mapper的自定义方法，直接传递Wrapper
+        userMapper.deductBalanceByIds(200, wrapper);
+    }
+
 
 }
