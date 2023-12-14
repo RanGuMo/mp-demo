a9f224c148b4b42223c29f533ffbaa8fba02c8d9
zack <1126146235@qq.com>
2023-12-14 12:45:54 +0800

    feat(核心功能-Service接口-批量新增): ✨ 开启&rewriteBatchedStatements=true 并用mybatisplus 提供的saveBatch方法 实现批量新增

--- src/test/java/com/itheima/mp/service/IUserServiceTest.java
+++ src/test/java/com/itheima/mp/service/IUserServiceTest.java
@@ -4,6 +4,8 @@ import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDateTime;
 
@@ -34,6 +36,53 @@ class IUserServiceTest {
         List<User> users = userService.listByIds(List.of(1L,2L,4L));
         users.forEach(System.out::println);
     }
+
+
+
+   //==========================测试批量新增=================================
+
+    @Test
+    void testSaveOneByOne() {
+        long b = System.currentTimeMillis();
+        for (int i = 1; i <= 100000; i++) { // 新增10万条数据
+            userService.save(buildUser(i));
+        }
+        long e = System.currentTimeMillis();
+        System.out.println("耗时：" + (e - b));
+    }
+
+    private User buildUser(int i) {
+        User user = new User();
+        user.setUsername("user_" + i);
+        user.setPassword("123");
+        user.setPhone("" + (18688190000L + i));
+        user.setBalance(2000);
+        user.setInfo("{\"age\": 24, \"intro\": \"英文老师\", \"gender\": \"female\"}");
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateTime(user.getCreateTime());
+        return user;
+    }
+
+
+    @Test
+    void testSaveBatch() {
+        // 准备10万条数据
+        List<User> list = new ArrayList<>(1000); // 创建一个容量为1000的空ArrayList
+
+        long b = System.currentTimeMillis(); // 记录开始时间
+        for (int i = 1; i <= 100000; i++) { // 循环100000次，即准备10万条数据
+            list.add(buildUser(i)); // 将buildUser(i)的返回结果添加到list中
+
+            // 每1000条批量插入一次
+            if (i % 1000 == 0) { // 当i是1000的倍数时
+                userService.saveBatch(list); // 调用userService的saveBatch方法，批量插入list中的数据
+                list.clear(); // 清空list，以便下一次批量插入
+            }
+        }
+        long e = System.currentTimeMillis(); // 记录结束时间
+        System.out.println("耗时：" + (e - b)); // 输出耗时时间
+    }
+
 
 
 }
