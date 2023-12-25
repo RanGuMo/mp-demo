0266cf49ffe30d534dd3ea8f830680c299321527
zack <1126146235@qq.com>
2023-12-25 09:29:03 +0800

    feat(拓展功能-逻辑删除): ✨ 逻辑删除的使用

---
+++ src/test/java/com/itheima/mp/mapper/AddressMapperTest.java
@@ -0,0 +1,30 @@
+package com.itheima.mp.mapper;
+
+
+import com.itheima.mp.domain.po.Address;
+import com.itheima.mp.service.IAddressService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@SpringBootTest
+class AddressMapperTest {
+
+    @Resource
+    private IAddressService addressService;
+
+    @Test
+    void testDeleteByLogic() {
+        // 删除方法与以前没有区别
+        addressService.removeById(59L);
+    }
+
+    @Test
+    void testQuery() {
+        List<Address> list = addressService.list();
+        list.forEach(System.out::println);
+    }
+
+}
